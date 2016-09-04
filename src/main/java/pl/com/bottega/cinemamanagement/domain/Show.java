77f7f2deca9f559b3cec4acba88f4c597a3cf2f6
arkadiuszarak <arak.arkadiusz@gmail.com>
2016-09-04 12:37:02 +0200

    domain layer

---
+++ src/main/java/pl/com/bottega/cinemamanagement/domain/Show.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.cinemamanagement.domain;
+
+import java.util.Date;
+
+/**
+ * Created by arkadiuszarak on 04/09/2016.
+ */
+public class Show {
+
+    private Cinema cinema;
+    private Movie movie;
+    private Date date;
+}
