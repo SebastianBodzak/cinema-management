26ca34259aacb93547aa2e6ca1e3ad1580c8b1bf
SebastianBodzak <sebastianbodzak@yahoo.com>
2016-09-04 13:13:37 +0200

    Add Cinema BackLog (ui + api + annotations)

--- src/main/java/pl/com/bottega/cinemamanagement/api/AdminPanel.java
+++ src/main/java/pl/com/bottega/cinemamanagement/api/AdminPanel.java
@@ -1,12 +1,26 @@
 package pl.com.bottega.cinemamanagement.api;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.com.bottega.cinemamanagement.domain.Cinema;
+import pl.com.bottega.cinemamanagement.domain.CinemaRepository;
+
 /**
  * Created by Dell on 2016-09-04.
  */
+@Service
 public class AdminPanel {
 
+    private CinemaRepository cinemaRepository;
+
+    public AdminPanel(CinemaRepository cinemaRepository) {
+        this.cinemaRepository = cinemaRepository;
+    }
+
+    @Transactional
     public void createCinema(CreateCinemaRequest request) {
-        //todo
+        Cinema cinema = new Cinema(request.getCinemaDto().getName(), request.getCinemaDto().getCity());
+        cinemaRepository.save(cinema);
     }
 
     public void createMovie(CreateMovieRequest request) {
