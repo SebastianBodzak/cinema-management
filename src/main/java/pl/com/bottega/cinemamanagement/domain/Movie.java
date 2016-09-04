18ab912afd88cc0e99cd3dc851074bb95ffd20a7
Ulvarin <ulvarpl@gmail.com>
2016-09-04 11:57:20 +0200

    Movie class

---
+++ src/main/java/pl/com/bottega/cinemamanagement/domain/Movie.java
@@ -0,0 +1,17 @@
+package pl.com.bottega.cinemamanagement.domain;
+
+/**
+ * Created by ulvar on 04.09.2016.
+ */
+public class Movie {
+    private String title;
+    private String description;
+    private String actors;
+    private String genres;
+    private int minAge;
+    private int length;
+
+
+
+
+}
