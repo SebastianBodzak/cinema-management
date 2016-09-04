7512e92994a0a4d79a03a9df37f9ec8435d77b3d
Ulvarin <ulvarpl@gmail.com>
2016-09-04 11:36:28 +0200

    adminPanelMergeTest

---
+++ src/main/java/pl/com/bottega/cinemamanagement/api/AdminPanel.java
@@ -0,0 +1,12 @@
+package pl.com.bottega.cinemamanagement.api;
+
+/**
+ * Created by ulvar on 04.09.2016.
+ */
+public class AdminPanel {
+
+    int admin = 1;
+    public int addAdmin(){
+        return admin*2;
+    }
+}
