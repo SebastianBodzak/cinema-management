6c1773e1ea7e2ded32fee17dc873d91a5506f005
bartosz.paszkowski <bartula87@gmail.com>
2016-09-04 11:38:11 +0200

    create cinema class

---
+++ src/main/java/pl/com/bottega/cinemamanagement/domain/Cinema.java
@@ -0,0 +1,15 @@
+package pl.com.bottega.cinemamanagement.domain;
+
+/**
+ * Created by bartosz.paszkowski on 04.09.2016.
+ */
+public class Cinema {
+
+    private String name;
+    private String city;
+
+    public Cinema(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+}
