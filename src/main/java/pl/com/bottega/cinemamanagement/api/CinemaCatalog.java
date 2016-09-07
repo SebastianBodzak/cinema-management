3c8a6d6de099a50f7d1d6267bb87e57aa36cc9b0
Ulvarin <ulvarpl@gmail.com>
2016-09-07 19:48:16 +0200

    listAll

--- src/main/java/pl/com/bottega/cinemamanagement/api/CinemaCatalog.java
+++ src/main/java/pl/com/bottega/cinemamanagement/api/CinemaCatalog.java
@@ -3,5 +3,9 @@ package pl.com.bottega.cinemamanagement.api;
 /**
  * Created by ulvar on 07.09.2016.
  */
-public interface CinemaCatalog  {
+public interface CinemaCatalog {
+
+    CinemaCatalog listAll(ListAllCinemasResponse listAllCinemasResponse);
+
+
 }
--- src/main/java/pl/com/bottega/cinemamanagement/infrastructure/JPACinemaCatalog.java
+++ src/main/java/pl/com/bottega/cinemamanagement/infrastructure/JPACinemaCatalog.java
@@ -1,7 +1,15 @@
 package pl.com.bottega.cinemamanagement.infrastructure;
 
+import pl.com.bottega.cinemamanagement.api.CinemaCatalog;
+import pl.com.bottega.cinemamanagement.api.ListAllCinemasResponse;
+
 /**
  * Created by ulvar on 07.09.2016.
  */
-public class JPACinemaCatalog {
+public class JPACinemaCatalog implements CinemaCatalog {
+
+    @Override
+    public CinemaCatalog listAll(ListAllCinemasResponse listAllCinemasResponse) {
+        return null; //TODO
+    }
 }
