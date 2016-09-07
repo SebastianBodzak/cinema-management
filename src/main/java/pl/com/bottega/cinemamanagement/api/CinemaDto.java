e01d587e0bad1d052213d6f5bf03ae6fa29c48fc
Ulvarin <ulvarpl@gmail.com>
2016-09-07 19:24:33 +0200

    listAllCinemas classes

---
+++ src/main/java/pl/com/bottega/cinemamanagement/api/CinemaDto.java
@@ -0,0 +1,44 @@
+package pl.com.bottega.cinemamanagement.api;
+
+/**
+ * Created by ulvar on 07.09.2016.
+ */
+public class CinemaDto {
+
+    private Long id;
+    private String name;
+    private String city;
+
+    public CinemaDto(Long id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+}
