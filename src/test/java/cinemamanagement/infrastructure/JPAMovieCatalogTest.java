309ce2235a75dbf2d9b582cb910d294125e0175f
arkadiuszarak <arak.arkadiusz@gmail.com>
2016-09-24 08:49:13 +0200

    JpaMovieCatalog added tests

--- src/test/java/cinemamanagement/infrastructure/JPAMovieCatalogTest.java
+++ src/test/java/cinemamanagement/infrastructure/JPAMovieCatalogTest.java
@@ -18,10 +18,15 @@ import pl.com.bottega.cinemamanagement.api.MovieCatalog;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by arkadiuszarak on 21/09/2016.
  */
@@ -29,7 +34,6 @@ import static org.junit.Assert.*;
 @ContextConfiguration("/application.xml")
 @TestPropertySource({"/jdbc-test.properties", "/hibernate-test.properties"})
 @WebAppConfiguration
-//@Sql("/fixtures/movieshow.sql")
 public class JPAMovieCatalogTest {
 
     @Autowired
@@ -40,19 +44,34 @@ public class JPAMovieCatalogTest {
 
     private LocalDate testDate = LocalDate.of(2016, 9 ,20);
     private Long testCienmaId = 2L;
+    private String anyTitle = "any title";
+    private String anyDescription = "any description";
+    private Long showId = 12L;
+    private Integer minAge = 18;
+    private Integer length = 120;
+    private Set<String> actors = new HashSet<String>(){{add("Himilsbach"); }};
+    private LocalDateTime anyDate = LocalDateTime.of(2016, 9 ,20, 13, 05, 00);
 
     @Sql("/fixtures/movieshow.sql")
     @Test
     @Transactional
     public void shouldlistMoviesInCinema(){
-        ListMoviesInCinemaResponse response = jpaMovieCatalog.listMoviesInCinema(testCienmaId, testDate);
 
-        assertTrue(response.getMovies().size() == 1);
+        ListMoviesInCinemaResponse response = jpaMovieCatalog.listMoviesInCinema(testCienmaId, testDate);
 
         List<ListMoviesInCinemaResponse.MovieDto> movieDtos = Lists.newArrayList(response.getMovies());
         ListMoviesInCinemaResponse.MovieDto dto = movieDtos.get(0);
+
         List<ListMoviesInCinemaResponse.ShowDto> showDtos = Lists.newArrayList(dto.getShows());
         ListMoviesInCinemaResponse.ShowDto showDto = showDtos.get(0);
-        assertEquals(testDate, showDto.getTime());
+
+        assertTrue(response.getMovies().size() == 1);
+        assertEquals(anyTitle, dto.getTitle());
+        assertEquals(anyDescription, dto.getDescription());
+        assertEquals(minAge,dto.getMinAge());
+        assertEquals(length, dto.getLength());
+        assertEquals(actors, dto.getActors());
+        assertEquals(showId, showDto.getId());
+        assertEquals(anyDate.toLocalTime(), showDto.getTime());
     }
 }
