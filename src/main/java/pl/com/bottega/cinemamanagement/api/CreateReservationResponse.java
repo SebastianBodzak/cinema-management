78f916196667c912686bd114a1df6921e1c21489
bartosz.paszkowski <bartula87@gmail.com>
2016-09-25 10:53:31 +0200

    Make Reservation draft 2/2

---
+++ src/main/java/pl/com/bottega/cinemamanagement/api/CreateReservationResponse.java
@@ -0,0 +1,17 @@
+package pl.com.bottega.cinemamanagement.api;
+
+/**
+ * Created by bartosz.paszkowski on 25.09.2016.
+ */
+public class CreateReservationResponse {
+
+    private String ReservationNumber;
+
+    public String getReservationNumber() {
+        return ReservationNumber;
+    }
+
+    public void setReservationNumber(String reservationNumber) {
+        ReservationNumber = reservationNumber;
+    }
+}
