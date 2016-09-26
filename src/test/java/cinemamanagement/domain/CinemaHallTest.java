eb3658ccf07bebeffd868fc026d2358ab7ab3c81
Ulvarin <ulvarpl@gmail.com>
2016-09-26 13:40:35 +0200

    CinemaHallTest update

--- src/test/java/cinemamanagement/domain/CinemaHallTest.java
+++ src/test/java/cinemamanagement/domain/CinemaHallTest.java
@@ -19,6 +19,7 @@ import static junit.framework.TestCase.assertTrue;
 public class CinemaHallTest {
 
     private Set<Reservation> reservations = new HashSet<>();
+    private Set<Reservation> emptyReservations = new HashSet<>();
     private CinemaHall cinemaHall;
 
     @Before
@@ -29,6 +30,26 @@ public class CinemaHallTest {
         reservations.add(reservation2);
 
     }
+
+    @Test
+    public void shouldReserveFullRow(){   //TODO tu nie powinno przejśc raczej, bo test poniżej wywala to czemu tu nagle działa?
+        cinemaHall = new CinemaHall(emptyReservations);
+
+        boolean result = cinemaHall.checkIfSeatsCanBeReserved(Sets.newHashSet(new Seat(1, 1), new Seat(1, 2), new Seat(1, 3),
+                new Seat(1, 4), new Seat(1, 5), new Seat(1, 6), new Seat(1, 7), new Seat(1, 8), new Seat(1, 9), new Seat(1, 10),
+                new Seat(1, 11), new Seat(1, 12), new Seat(1, 13), new Seat(1, 14), new Seat(1, 15)));  //TODO no i przepuszcza miejsca ktore jużsą zarezerwowane 1,12 1,13
+        assertTrue(result);  //TODO Jak test leci sam to przechodzi, jak leci cała klasa testów to wywala out of bound ex row/seat.
+    }
+
+    @Test
+    public void shouldReserveLastRowAndSeat(){
+        cinemaHall = new CinemaHall(reservations);
+
+        boolean result = cinemaHall.checkIfSeatsCanBeReserved(Sets.newHashSet(new Seat(10, 15))); //TODO ROW=10 - out of bound?? seat=15 - out of bound exception??
+        assertTrue(result);
+    }
+
+
 
     @Test
     public void shouldCheckIfSeatsCanBeReserved() {
@@ -42,9 +63,32 @@ public class CinemaHallTest {
     @Test
     public void shouldFailToMakeReservationBecauseItsAlreadyReserved() {
         cinemaHall = new CinemaHall(reservations);
-        boolean result = cinemaHall.checkIfSeatsCanBeReserved(Sets.newHashSet(new Seat(1, 12), new Seat(1, 9), new Seat(1, 8)));
+        boolean result = cinemaHall.checkIfSeatsCanBeReserved(Sets.newHashSet(new Seat(1, 12), new Seat(1, 13), new Seat(1, 8))); //TODO czemu z 1,8 jest ok, a z 1,14 już nie?
         assertFalse(result);
     }
+
+    @Test
+    public void shouldFailToReserveDueToDifferentRows() {
+        cinemaHall = new CinemaHall(reservations);
+        boolean result = cinemaHall.checkIfSeatsCanBeReserved(Sets.newHashSet(new Seat(1, 4), new Seat(6, 5)));
+        assertFalse(result);
+
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void shouldFailToReserveBecauseOfNonExistingSeats() {
+        cinemaHall = new CinemaHall(reservations);
+        cinemaHall.checkIfSeatsCanBeReserved(Sets.newHashSet(new Seat(1, 16), new Seat(11, 9)));
+
+    }
+
+    @Test
+    public void shouldFailToReserveDueSeatsNotNextToEachOther() {
+        cinemaHall = new CinemaHall(reservations);
+        boolean result = cinemaHall.checkIfSeatsCanBeReserved(Sets.newHashSet(new Seat(1, 10), new Seat(1, 11), new Seat(1, 14)));
+        assertFalse(result);
+    }
+
 
     private Reservation createReservation(Seat... seat) {
         Set<TicketOrder> ticketOrders = new HashSet<>(Arrays.asList(new TicketOrder("regular", 10), new TicketOrder("student", 15)));
@@ -55,6 +99,6 @@ public class CinemaHallTest {
         for (Seat s : seat)
             seats.add(s);
 
-        return new Reservation(show , ticketOrders, seats, customer, totalPrice);
+        return new Reservation(show, ticketOrders, seats, customer, totalPrice);
     }
 }
--- src/test/java/cinemamanagement/domain/ReservationTest.java
+++ src/test/java/cinemamanagement/domain/ReservationTest.java
@@ -9,7 +9,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by ulvar on 25.09.2016.
@@ -22,13 +21,12 @@ public class ReservationTest {
         Set<Seat> seats = new HashSet<>(Arrays.asList(new Seat(2, 2), new Seat(3, 3)));
         Customer customer = new Customer("jan", "kowalski", "mail@mail.com", "+486439543039");
         BigDecimal totalPrice = new BigDecimal(100);
-
-        Reservation reservation = new Reservation(ticketOrders, seats, customer,totalPrice);
+        Show show = new Show();
+        Reservation reservation = new Reservation(show, ticketOrders, seats, customer, totalPrice);
 
         assertEquals(ticketOrders, reservation.getTicketsOrder());
         assertEquals(seats, reservation.getSeats());
         assertEquals(customer, reservation.getCustomer());
         assertEquals(ReservationStatus.PENDING, reservation.getStatus());
-        assertNotNull(reservation.getNumber());
     }
 }
