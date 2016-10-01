38431215a070421b013f85a4c8903b881dd15f6c
Ulvarin <ulvarpl@gmail.com>
2016-10-01 13:41:27 +0200

    collect Payment Draft 1/2

--- src/main/java/pl/com/bottega/cinemamanagement/infrastructure/EmailFacadeImpl.java
+++ src/main/java/pl/com/bottega/cinemamanagement/infrastructure/EmailFacadeImpl.java
@@ -1,12 +1,12 @@
 package pl.com.bottega.cinemamanagement.infrastructure;
 
-import pl.com.bottega.cinemamanagement.api.EmailFasade;
+import pl.com.bottega.cinemamanagement.api.EmailFacade;
 import pl.com.bottega.cinemamanagement.domain.Reservation;
 
 /**
  * Created by ulvar on 01.10.2016.
  */
-public class EmailFasadeImpl implements EmailFasade{
+public class EmailFacadeImpl implements EmailFacade {
     @Override
     public void sendTickets(Reservation reservation) {
 
--- src/main/java/pl/com/bottega/cinemamanagement/infrastructure/PdfFacadeImpl.java
+++ src/main/java/pl/com/bottega/cinemamanagement/infrastructure/PdfFacadeImpl.java
@@ -1,6 +1,6 @@
 package pl.com.bottega.cinemamanagement.infrastructure;
 
-import pl.com.bottega.cinemamanagement.api.PdfFasade;
+import pl.com.bottega.cinemamanagement.api.PdfFacade;
 import pl.com.bottega.cinemamanagement.domain.Reservation;
 
 import java.io.File;
@@ -8,7 +8,7 @@ import java.io.File;
 /**
  * Created by ulvar on 01.10.2016.
  */
-public class PdfFasadeImpl implements PdfFasade {
+public class PdfFacadeImpl implements PdfFacade {
     @Override
     public File createPdf(Reservation reservation) {
         return null;
